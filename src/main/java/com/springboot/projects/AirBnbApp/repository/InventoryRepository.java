19168433a2977f986dbe2ce82bc0bf39987cd960
souvik saha <souviksaha@crldomain.com>
2026-04-22 17:44:10 +0530

    Updated the search feature of hotels

--- src/main/java/com/springboot/projects/AirBnbApp/repository/InventoryRepository.java
+++ src/main/java/com/springboot/projects/AirBnbApp/repository/InventoryRepository.java
@@ -1,11 +1,35 @@
 package com.springboot.projects.AirBnbApp.repository;
 
+import com.springboot.projects.AirBnbApp.entity.Hotel;
 import com.springboot.projects.AirBnbApp.entity.Inventory;
 import com.springboot.projects.AirBnbApp.entity.Room;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     void deleteByRoom(Room room);
+
+    @Query("""
+            SELECT DISTINCT i.hotel
+            FROM INVENTORY i
+            WHERE i.city = :city
+                AND i.date BETWEEN :startDate AND :endDate
+                AND i.closed = FALSE
+                AND (i.totalCount - i.bookedCount) >= roomsCount
+            GROUP BY i.hotel, i.room
+            HAVING COUNT(i.date) = :dateCount
+            """)
+    Page<Hotel> findHotelsWithAvailableInventory(
+            @Param("city") String city,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("roomsCount") Integer roomsCount,
+            @Param("dateCount") Long dateCount,
+            Pageable pageable
+    );
 }
--- src/main/java/com/springboot/projects/AirBnbApp/service/Impl/InventoryServiceImpl.java
+++ src/main/java/com/springboot/projects/AirBnbApp/service/Impl/InventoryServiceImpl.java
@@ -2,12 +2,14 @@ package com.springboot.projects.AirBnbApp.service.Impl;
 
 import com.springboot.projects.AirBnbApp.dto.HotelDto;
 import com.springboot.projects.AirBnbApp.dto.HotelSearchRequest;
+import com.springboot.projects.AirBnbApp.entity.Hotel;
 import com.springboot.projects.AirBnbApp.entity.Inventory;
 import com.springboot.projects.AirBnbApp.entity.Room;
 import com.springboot.projects.AirBnbApp.repository.InventoryRepository;
 import com.springboot.projects.AirBnbApp.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @Slf4j
@@ -22,6 +25,7 @@ import java.time.LocalDate;
 public class InventoryServiceImpl implements InventoryService {
 
     private final InventoryRepository inventoryRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public void initializeRoomForAYear(Room room) {
@@ -53,6 +57,15 @@ public class InventoryServiceImpl implements InventoryService {
     public Page<HotelDto> searchHotels(HotelSearchRequest hotelSearchRequest) {
         Pageable pageable = PageRequest.of(hotelSearchRequest.getPage(), hotelSearchRequest.getSize());
         // Criteria for inventory: 1. startDate <= date <= endDate   2. city   3. availability: (totalCount - bookedCount) >= roomsCount
-        return null;
+        long dateCount = ChronoUnit.DAYS.between(hotelSearchRequest.getStartDate(), hotelSearchRequest.getEndDate()) + 1;
+        Page<Hotel> hotelPage = inventoryRepository.findHotelsWithAvailableInventory(
+                hotelSearchRequest.getCity(),
+                hotelSearchRequest.getStartDate(),
+                hotelSearchRequest.getEndDate(),
+                hotelSearchRequest.getRoomsCount(),
+                dateCount,
+                pageable
+        );
+        return hotelPage.map((element) -> modelMapper.map(element, HotelDto.class));
     }
 }
